c37eafab9689030473be77baa6bf74e42d80929e
Alex A. Kwashie <alex.kwashie@hotmail.com>
2017-08-28 23:11:39 +0100

    continue

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 //listen for form submit
 
-document.getElementbyId('myForm').addEventListener('submit')
+document.getElementById('myForm').addEventListener('submit', saveBooking);
+
+//Save bookmark
+function saveBooking(e){
+var siteName = document.getElementById('siteName').value;
+var siteUrl = document.getElementById('siteUrl').value;
+
+console.log(siteName);
+
+//prevent form from submitting
+e.preventDefault();
+
+}
