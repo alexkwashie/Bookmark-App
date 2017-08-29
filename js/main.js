3d1dae3d1455f086b149efbbfbcb4a31e3bc959e
Alex A. Kwashie <alex.kwashie@hotmail.com>
2017-08-29 23:25:46 +0100

    JS Section 1
    JS Section 1

--- js/main.js
+++ js/main.js
@@ -5,10 +5,33 @@ document.getElementById('myForm').addEventListener('submit', saveBooking);
 //Save bookmark
 function saveBooking(e){
 var siteName = document.getElementById('siteName').value;
-var siteUrl = document.getElementById('siteUrl').value;
+var siteU = document.getElementById('siteUrl').value;
 
-console.log(siteName);
+var bookmark = {
+name: siteName,
+Url: siteU
+}
 
+/*
+//local storage test
+localStorage.setItem('test', 'Newfile');
+console.log(localStorage.getItem('test'));
+localStorage.removeItem('test');
+console.log(localStorage.getItem('test'));
+*/
+
+//Test if bookmark is null
+if (localStorage.getItem('bookmarks') === null){
+//initiate array
+  var bookmarks = [];
+// this adds to bookmark to the array from
+localStorage.push('bookmark');
+//Set to local storage and changes the set up to  string
+localStorage.setItem('bookmarks', JSON.stringify(bookmark));
+
+
+
+}
 //prevent form from submitting
 e.preventDefault();
 
