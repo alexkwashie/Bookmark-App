c9ce9f5ce7614a3bcbbca65e3a9589132e261b89
Alex A. Kwashie <alex.kwashie@hotmail.com>
2017-08-30 15:27:39 +0100

    final project

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ function saveBookmark(e){
   var siteName =document.getElementById('siteName').value;
   var siteUrl =document.getElementById('siteUrl').value;
 
+if (!siteName || !siteUrl){
+  alert('Please fill in the from');
+  return false;
+}
+var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+var regex = new RegExp(expression);
+
+if (!siteUrl.match(regex)){
+  alert('Please enter a valid URL');
+  return false;
+}
   var bookmark = {
     name: siteName,
     url: siteUrl
@@ -36,13 +47,34 @@ function saveBookmark(e){
     // Re-set back to localStorage
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
+  //Re-fetch bookmarks
+    fetchBookmarks()
 
-  // Clear form
+  // Clear the form
   document.getElementById('myForm').reset();
 
   // Prevent form from submitting
   e.preventDefault();
 }
+
+//delete Bookmark function
+function deleteBookmark(url){
+  //Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  //loop through url
+  for (i = 0; i < bookmarks.length; i++){
+    //Remove bookmark from array
+    if(bookmarks[i].url == url){
+      bookmarks.splice(i, 1);
+    }
+  }
+  // Set to localStorage and convt to a JSON string
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+//Re-fetch bookmarks
+  fetchBookmarks()
+}
+
 
 //fetch Bookmark
 function fetchBookmarks(){
@@ -51,5 +83,19 @@ function fetchBookmarks(){
 //Get output Id from html
   var bookmarkResults = document.getElementById('bookmarksResults');
 
-  JSON.stringify()
+  //build output
+  bookmarkResults.innerHTML = '';
+  for (var i = 0; i < bookmarks.length; i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarkResults.innerHTML += '<div class="alert alert-warning">' +
+                                 '<h3>'+name+
+/*_blank - open in another tab*/ ' <a class="btn btn-primary" target="_blank" href="'+url+'" style="margin-right:50px">Visit</a> '+
+                                 ' <a onclick ="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#" style="float:right">Delete</a> '+
+                                 '</h3>'+
+                                 '</div>';
+
+
+  }
 }
